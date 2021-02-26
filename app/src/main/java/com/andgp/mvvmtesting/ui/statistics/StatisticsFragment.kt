ed1bb2e0216dbcf4729430148bf37e3c3472bca1
Andres Gonzalez <andres.paez.1885@gmail.com>
2021-02-26 12:05:55 -0600

    Complete Statistics Screen
    This is the last Screen before moving to the next Advance Test Kotlin.

--- app/src/main/java/com/andgp/mvvmtesting/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/andgp/mvvmtesting/ui/statistics/StatisticsFragment.kt
@@ -1,11 +1,46 @@
 package com.andgp.mvvmtesting.ui.statistics
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.andgp.mvvmtesting.R
+import com.andgp.mvvmtesting.databinding.FragmentStatisticsBinding
+import com.andgp.mvvmtesting.util.setupRefreshLayout
 
 /**
  *  Created by Andres Gonzalez on 2/26/21.
  *  Copyright (c) 2020 City Electric Supply. All rights reserved.
  */
 class StatisticsFragment : Fragment() {
+    private lateinit var viewDataBinding: FragmentStatisticsBinding
 
+    private val viewModel by viewModels<StatisticsViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_statistics,
+            container,
+            false
+        )
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewDataBinding.let {
+            it.viewmodel = viewModel
+            it.lifecycleOwner = viewLifecycleOwner
+        }
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+
+    }
 }
--- app/src/main/java/com/andgp/mvvmtesting/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/andgp/mvvmtesting/ui/statistics/StatisticsViewModel.kt
@@ -1,12 +1,50 @@
 package com.andgp.mvvmtesting.ui.statistics
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.*
+import com.andgp.mvvmtesting.data.Result
+import com.andgp.mvvmtesting.data.Result.Success
+import com.andgp.mvvmtesting.data.source.DefaultTasksRepository
+import com.andgp.mvvmtesting.data.source.model.Task
+import kotlinx.coroutines.launch
 
 /**
  *  Created by Andres Gonzalez on 2/26/21.
  *  Copyright (c) 2020 City Electric Supply. All rights reserved.
  */
 class StatisticsViewModel(application: Application) : AndroidViewModel(application) {
+    private val tasksRepository = DefaultTasksRepository.getRepository(application)
+
+    private val _dataLoading = MutableLiveData<Boolean>(false)
+    val dataLoading = _dataLoading
+
+    private val tasks: LiveData<Result<List<Task>>> = tasksRepository.observeTasks()
+
+    private val stats: LiveData<StatsResult?> = tasks.map {
+        if (it is Success) {
+            getActiveAndCompleteState(it.data)
+        } else {
+            null
+        }
+    }
+
+    val activeTasksPercent = stats.map {
+        it?.activeTasksPercent ?: 0f
+    }
+    val completePercent = stats.map {
+        it?.completeTasksPercent ?: 0f
+    }
+
+    val error: LiveData<Boolean> = tasks.map { it is Error }
+
+    val empty: LiveData<Boolean> = tasks.map { (it as? Success)?.data.isNullOrEmpty() }
+
+    fun refresh() {
+        _dataLoading.value = true
+        viewModelScope.launch {
+            tasksRepository.refreshTasks()
+            _dataLoading.value = false
+        }
+    }
 
 }
